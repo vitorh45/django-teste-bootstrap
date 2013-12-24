8260abf44380173059511e0391ff7745a7b709d8
vitor hugo campos <vitorh45@gmail.com>
2013-12-24 11:14:49 -0200

    removendo o settings local

--- blog/settings_local.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vitor.campos007@gmail.com'
-EMAIL_HOST_PASSWORD = 'slashiter'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True            # Set to empty string for default. Not used with sqlite3.
